d86914cfd96f66b42bb4ac27d922efd6c00a38e7
kavinduaj12 <86953304+kavinduaj12@users.noreply.github.com>
2021-07-19 00:23:29 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,8 +11,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Kavindu AJ",
+  title: "Hi all, I'm Kav!ndu",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
@@ -24,13 +24,13 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/kavinduaj12",
+  linkedin: "#",
+  gmail: "kavinduavishka79@gmail.com",
+  gitlab: "#",
+  facebook: "#",
+  medium: "#",
+  stackoverflow: "#",
   // Instagram and Twitter are also supported in the links!
   display: true // Set true to display this section, defaults to false
 };
